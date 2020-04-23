c96981dd8259b946b7883c8912cc820ef56be066
Nandor Biro <dev@nandito.info>
2020-04-23 06:49:53 +0200

    chore: display "welcome" message when app starts
    Use process.nextTick() to avoid `undefined` error

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ class Server extends EventEmitter {
   constructor(client) {
     super()
 
+    // we need to set this emitter on the next process tick
+    // because server.on('response', cb) is defined AFTER this in the client.js
+    // so our handler for this `response` event will be not defined yet.
+    process.nextTick(() => {
+      this.emit('response', ('Type a command (help to list commands'))
+    })
+
     client.on('command', (command) => {
       switch (command) {
         case 'help':
